c52423f2c95e4438376a3883c151f791ffae61e5
Jonatas Fabricio da Silva <joncleanmgr@gmail.com>
2022-10-05 15:27:02 -0300

    update CarroController and CarroService

--- src/main/java/com/example/carros/api/CarroController.java
+++ src/main/java/com/example/carros/api/CarroController.java
@@ -2,6 +2,7 @@ package com.example.carros.api;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,7 +13,9 @@ import com.example.carros.api.domain.CarroService;
 @RestController // TRANSFORMA ESSA CLASSE EM UM WEB SERVICE REST
 @RequestMapping("/api/v1/carros")//DEFINE PARA ONDE O WEB SERVICE ESTA MAPEADO
 public class CarroController {
-	public CarroService service = new CarroService();
+	
+	@Autowired
+	private CarroService service;
 	
 	@GetMapping //METODO PARA TRAZER ALGO
 	public List<Carro> getAll() {
