9934a05c754fb3de92ee29595645f23e65ee957f
Jonatas Fabricio da Silva <joncleanmgr@gmail.com>
2022-10-10 09:58:56 -0300

    create tests

--- src/test/java/com/example/carros/CarrosApplicationTests.java
+++ src/test/java/com/example/carros/CarrosApplicationTests.java
@@ -1,10 +1,15 @@
 package com.example.carros;
 
+import com.example.carros.api.domain.CarroService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CarrosApplicationTests {
+
+	@Autowired
+	private CarroService service;
 
 	@Test
 	void contextLoads() {
