3c1d676a4b8961f0e9c37191184c6aff679dc551
Jonatas Fabricio da Silva <joncleanmgr@gmail.com>
2022-10-10 16:43:57 -0300

    .

--- src/main/java/com/example/carros/api/CarroController.java
+++ src/main/java/com/example/carros/api/CarroController.java
@@ -1,25 +1,19 @@
 package com.example.carros.api;
 
+import com.example.carros.api.domain.Carro;
+import com.example.carros.api.domain.CarroService;
+import com.example.carros.api.domain.dto.CarroDTO;
+import io.swagger.annotations.Api;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
 import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
-import com.example.carros.api.domain.Carro;
-import com.example.carros.api.domain.CarroService;
-import com.example.carros.api.domain.dto.CarroDTO;
-
+@Api(value = "CarroController")
 @RestController // TRANSFORMA ESSA CLASSE EM UM WEB SERVICE REST
 @RequestMapping("/api/v1/carros") // DEFINE PARA ONDE O WEB SERVICE ESTA MAPEADO
 public class CarroController {
