bbf17a10e71ff24df3a47c4d7e18cf405b22329c
Jonatas Fabricio da Silva <joncleanmgr@gmail.com>
2022-10-13 09:29:38 -0300

    .

--- src/main/java/com/example/carros/api/IndexController.java
+++ src/main/java/com/example/carros/api/IndexController.java
@@ -1,5 +1,7 @@
 package com.example.carros.api;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +13,11 @@ public class IndexController {
 	@GetMapping //METODO PARA TRAZER ALGO
 	public String get() {
 		return "API DOS CARROS!!!";
+	}
+
+	@GetMapping("/userInfo")
+	public UserDetails userInfo(@AuthenticationPrincipal UserDetails user){
+		return user;
 	}
 	
 }
