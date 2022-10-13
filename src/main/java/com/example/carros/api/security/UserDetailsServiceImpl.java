7d709e7f270f85adf67c8b141812cf1e69fd4683
Jonatas Fabricio da Silva <joncleanmgr@gmail.com>
2022-10-13 10:20:03 -0300

    .

--- src/main/java/com/example/carros/api/security/UserDetailsServiceImpl.java
+++ src/main/java/com/example/carros/api/security/UserDetailsServiceImpl.java
@@ -1,23 +1,26 @@
 package com.example.carros.api.security;
 
+import com.example.carros.api.domain.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service(value = "userDetailsService")
 public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 
-        if(username.equals("user")){
-            return User.withUsername(username).password(encoder.encode("user")).roles("USER").build();
-        } else if (username.equals("admin")) {
-            return User.withUsername(username).password(encoder.encode("password")).roles("USER", "ADMIN").build();
+        com.example.carros.api.domain.User user = userRepository.findByLogin(username);
+        if(user == null){
+            throw new UsernameNotFoundException("user not found");
         }
-        throw new UsernameNotFoundException("user not found");
+        return User.withUsername(username).password(user.getSenha()).roles("USER").build();
+
     }
 }
