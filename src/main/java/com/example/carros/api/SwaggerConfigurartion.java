e74f17b9c809b86b088747987c0882c7b1f1049c
Jonatas Fabricio da Silva <joncleanmgr@gmail.com>
2022-10-14 09:26:02 -0300

    ^.

---
+++ src/main/java/com/example/carros/api/SwaggerConfigurartion.java
@@ -0,0 +1,29 @@
+package com.example.carros.api;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class SwaggerConfigurartion {
+
+    @Bean
+    public Docket api(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.ant("/api/**"))
+                .build()
+                .apiInfo(apiInfo());
+    }
+    private ApiInfo apiInfo(){
+        return new ApiInfoBuilder()
+                .contact(new Contact("","","")).title("").description("").license("").licenseUrl("").version("").build();
+    }
+}
